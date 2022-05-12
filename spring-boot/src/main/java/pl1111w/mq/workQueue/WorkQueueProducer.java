5c63930dacbe187152da28802121f2b60ae43881
xiaobo wei <1362596047@qq.com>
2022-05-12 14:45:36 +0800

    work_queue_confirm

--- spring-boot/src/main/java/pl1111w/mq/workQueue/WorkQueueProducer.java
+++ spring-boot/src/main/java/pl1111w/mq/workQueue/WorkQueueProducer.java
@@ -21,6 +21,14 @@ public class WorkQueueProducer {
 
         Channel channel = RabbitChannel.getRabbitChannel();
         try {
+            /**
+             * 生成一个队列
+             * 1.队列名称
+             * 2.队列里面的消息是否持久化 默认消息存储在内存中
+             * 3.该队列是否只供一个消费者进行消费 是否进行共享 true 可以多个消费者消费
+             * 4.是否自动删除 最后一个消费者端开连接以后 该队列是否自动删除 true 自动删除
+             * 5.其他参数
+             */
             channel.queueDeclare(WORK_QUEUE, false, false, false, null);
             Scanner scanner = new Scanner(System.in);
             String message;
